7d09ea1e9bd5052c6fd287a05bdc5a72688d6c68
GANTA SAI KIRAN <163864060+SaiKiranGanta2003@users.noreply.github.com>
2024-10-11 10:34:14 +0530

    Add files via upload

---
+++ starter.js
@@ -0,0 +1,28 @@
+
+document.getElementById('dropbtn').addEventListener('click', function() {
+    const navLinks = document.getElementById('nav-links');
+    
+    // Toggle visibility of the nav links when the button is clicked
+    if (navLinks.style.display === 'block' || navLinks.style.display === 'flex') {
+        navLinks.style.display = 'none';
+       
+        console.log(navLinks.style.display)
+
+        console.log('hello')
+
+    } else {
+        navLinks.style.display = 'flex';
+        console.log(navLinks.style.display)
+        navLinks.style.bakgroundColor='yellow';
+        navLinks.style.position='absolute';
+        navLinks.style.top='50px';
+        navLinks.style.right='0';
+        navLinks.style.gap='20px';
+        navLinks.style.padding='10px';
+        navLinks.style.backgroundColor='white';
+        navLinks.style.borderBottom='2PX solid black';
+        navLinks.style.flexDirection='column';
+        console.log('bye')
+
+    }
+});
